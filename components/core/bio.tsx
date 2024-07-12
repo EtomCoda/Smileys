117d4af330ec4d3e2bcd15b9c3a55815a8f3b527
Etom <98264570+SageCoda@users.noreply.github.com>
2024-07-12 12:06:51 +0100

    bio component update

--- components/core/bio.tsx
+++ components/core/bio.tsx
@@ -1,4 +1,8 @@
-import { KeyboardAvoidingView, Platform,Button as RNButton } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Button as RNButton,
+} from "react-native";
 import { Text, View } from "@/components/Themed";
 import Form from "@/components/core/form";
 import Logo from "@/components/core/logo";
@@ -6,9 +10,9 @@ import Button from "@/components/core/button";
 import styles from "./tabstyles";
 import { useState, useEffect } from "react";
 import { Alert } from "react-native";
-import { createTable,db } from "./db";
+import { createTable, db } from "./db";
 import { router } from "expo-router";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Bio() {
   const [firstname, setFirstName] = useState("");
@@ -20,15 +24,18 @@ export default function Bio() {
 
   useEffect(() => {
     createTable();
- 
   }, []);
 
- 
-//saving data
+  //saving data
   const saveData = async () => {
-    if (firstname.length == 0 || middlename.length == 0 || lastname.length == 0 ||
-      dob.length == 0 || address.length == 0) {
-      Alert.alert("Please fill in all fields.Use 'none' where nothing applies")
+    if (
+      firstname.length == 0 ||
+      middlename.length == 0 ||
+      lastname.length == 0 ||
+      dob.length == 0 ||
+      address.length == 0
+    ) {
+      Alert.alert("Please fill in all fields.Use 'none' where nothing applies");
     } else {
       try {
         (await db).transaction(async (tx) => {
@@ -45,14 +52,14 @@ export default function Bio() {
         setDob("");
         setAddress("");
 
-        router.push('/two');
+        router.push("/two");
       } catch (error) {
         console.error(error);
         Alert.alert("Error saving data, please try again");
       }
     }
   };
-//changing date
+  //changing date
   const onDateChange = (event: any, selectedDate?: Date) => {
     setShowDatePicker(false);
     if (selectedDate) {
@@ -78,31 +85,24 @@ export default function Bio() {
         subcaption1="Enter firstname"
         onChangeText1={setFirstName}
         value1={firstname}
-
         caption2="MiddleName:"
         subcaption2="Enter middlename"
         onChangeText2={setMiddleName}
         value2={middlename}
-
         caption3="LastName:"
         subcaption3="Enter lastname"
         onChangeText3={setLastName}
         value3={lastname}
-
         caption4="Date of Birth:"
         subcaption4="DD/MM/YY"
         onChangeText4={setDob}
         value4={dob}
         onDatePress={() => setShowDatePicker(true)}
-
         caption5="Home Address:"
         subcaption5="Enter address"
         onChangeText5={setAddress}
         value5={address}
-
       />
-
-    
 
       {showDatePicker && (
         <DateTimePicker
@@ -115,7 +115,7 @@ export default function Bio() {
 
       <View style={styles.positionbutton}>
         <View style={styles.button}>
-          <Button title="Next" color={'#35524A'} onPress={saveData}></Button>
+          <Button title="Next" color={"#35524A"} onPress={saveData}></Button>
         </View>
       </View>
 
